386564c78d8e98402b35f34f59c57df7f4249e5c
Hsin Yu Liu <dewseileaf@gmail.com>
2023-01-18 11:15:23 +0100

    bug

--- bonus/src/ft_check_res.c
+++ bonus/src/ft_check_res.c
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 16:18:54 by sasha             #+#    #+#             */
-/*   Updated: 2023/01/17 16:22:52 by sasha            ###   ########.fr       */
+/*   Updated: 2023/01/18 11:13:46 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	ft_check_res(t_stack *stack)
 	int		len;
 	t_node	*a;
 
+	if (!ft_isempty(stack->b))
+	{
+		write(1, "KO\n", 3);
+		return ;
+	}
 	i = 1;
 	len = ft_lstlen(stack->a);
 	a = stack->a;
