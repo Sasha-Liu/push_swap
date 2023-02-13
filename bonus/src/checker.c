e1fba9d524e57ae02f955014bd269e906dbba441
Hsin-yu Liu <hsliu@z3r11p5.42lyon.fr>
2023-02-13 11:19:15 +0100

    leak

--- bonus/src/checker.c
+++ bonus/src/checker.c
@@ -6,11 +6,19 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 14:49:56 by sasha             #+#    #+#             */
-/*   Updated: 2023/01/26 10:36:49 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/02/13 11:18:44 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
+
+static void	ft_free_input(int res, char **input)
+{
+	if (res)
+	{
+		ft_free_2(input);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,21 +30,20 @@ int	main(int argc, char **argv)
 	n = argc - 1;
 	input = argv + 1;
 	res = ft_set_input(argc, argv, &n, &input);
-	if (!ft_is_int(n, input))
-	{
+	if (input == NULL)
 		return (0);
-	}
+	if (!ft_is_int(n, input))
+		return (ft_free_input(res, input), 0);
 	stack = ft_malloc_stack();
 	if (stack == NULL)
 	{
-		return (0);
+		return (ft_free_input(res, input), 0);
 	}
 	if (ft_init_stack(stack, n, input) == -1)
 	{
-		return (ft_free(&stack), 0);
+		return (ft_free(&stack), ft_free_input(res, input), 0);
 	}
 	ft_check(stack);
 	ft_free(&stack);
-	if (res)
-		ft_free_2(input);
+	ft_free_input(res, input);
 }
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,19 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:16:44 by hsliu             #+#    #+#             */
-/*   Updated: 2023/01/25 13:58:25 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/02/13 11:17:55 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	ft_free_input(int res, char **input)
+{
+	if (res)
+	{
+		ft_free_2(input);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,21 +30,20 @@ int	main(int argc, char **argv)
 	n = argc - 1;
 	input = argv + 1;
 	res = ft_set_input(argc, argv, &n, &input);
-	if (!ft_is_int(n, input))
-	{
+	if (input == NULL)
 		return (0);
-	}
+	if (!ft_is_int(n, input))
+		return (ft_free_input(res, input), 0);
 	stack = ft_malloc_stack();
 	if (stack == NULL)
 	{
-		return (0);
+		return (ft_free_input(res, input), 0);
 	}
 	if (ft_init_stack(stack, n, input) == -1)
 	{
-		return (ft_free(&stack), 0);
+		return (ft_free(&stack), ft_free_input(res, input), 0);
 	}
 	ft_sort(stack);
 	ft_free(&stack);
-	if (res)
-		ft_free_2(input);
+	ft_free_input(res, input);
 }
